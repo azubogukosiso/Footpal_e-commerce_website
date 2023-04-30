dd4d683104969d046eff670ac069807b1ff8c6d8
azuboguko@gmail.com <azuboguko@gmail.com>
2023-04-30 17:56:07 +0100

    Modified for cloud database

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const itemRouter = require("./routes/items");
 const stripe = require("./routes/stripe");
 
 // CONNECTION TO THE DATABASE
-mongoose.connect(process.env.MONGO_URL_LOCAL, {
+mongoose.connect(process.env.MONGO_URL_ATLAS, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
